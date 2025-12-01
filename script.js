a625fb9175c5d57de4b1ffba4597da884bc0e1fe
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-12-01 20:28:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 //your JS code here. If required.
 // List of sounds (make sure files exist in sounds folder)
 // List of sound names – files must exist in sounds/ folder
-const buttons = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+let currentAudio = null;
 
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    let soundName = btn.getAttribute("data-sound");
-
-    // stop all currently playing sounds
-    stopAllSounds();
-
-    // play sound
-    if (soundName !== "stop") {
-      const audio = new Audio(`sounds/${soundName}.mp3`);
-      audio.play();
-    }
-  });
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const soundFile = button.getAttribute('data-sound');
+        playSound(soundFile);
+    });
 });
 
-function stopAllSounds() {
-  const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
-  sounds.forEach(s => {
-    let audio = new Audio(`sounds/${s}.mp3`);
-    audio.pause();
-    audio.currentTime = 0;
-  });
+stopButton.addEventListener('click', () => {
+    stopSound();
+});
+
+function playSound(file) {
+    if (currentAudio) {
+        currentAudio.pause(); // Stop the current audio
+    }
+    currentAudio = new Audio(`sounds/${file}`);
+    currentAudio.play();
+}
+
+function stopSound() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0; // Reset to the start
+    }
 }
